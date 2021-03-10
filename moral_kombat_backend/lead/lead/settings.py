b0417866a664227079c302c1b4ff580742a272b3
Yi Wei <yiw00@outlook.com>
2021-03-10 19:45:38 +0000

    Add database to container

--- moral_kombat_backend/lead/lead/settings.py
+++ moral_kombat_backend/lead/lead/settings.py
@@ -95,10 +95,10 @@ DATABASES = {
         # 'NAME': BASE_DIR / 'db.sqlite3',
 
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'cnehcbso',
-        'USER': 'cnehcbso',
-        'PASSWORD': 'qy2xdb_zEcAZFOmY7fvQT1SddHRUhbCI',
-        'HOST': 'raja.db.elephantsql.com',
+        'NAME': 'ethicisim',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
         'PORT': '5432',
     }
 }

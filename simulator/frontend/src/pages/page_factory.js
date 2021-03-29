5ea0716531cb460f6acd64f8db8129e1581c9184
Jake Recoulle <42967542+jdrecoulle0001@users.noreply.github.com>
2021-03-29 18:24:22 -0400

    Created page factory to create pages returned from backend for a scenario.

--- simulator/frontend/src/pages/page_factory.js
+++ simulator/frontend/src/pages/page_factory.js
@@ -5,36 +5,35 @@ import Action from './action';
 import Stakeholders from './stakeholders'
 
 export default class Page {
-    constructor(visited, completed, pageNumber, type, nextPageNumber=1, prevPageNumber=1, content) {
+    constructor(visited, completed, pageNumber, type, nextPageNumber=1, title, content) {
         const pageTypes = {
             "A": (
                 <Action 
                     nextPageID={nextPageNumber}
-                    prevPageID={prevPageNumber}
+                    prevPageID={pageNumber - 1}
                     content={content}        
                 />),
             "G": (
                 <Introduction 
                     nextPageNumber={nextPageNumber}
-                    prevPageNumber={prevPageNumber}
+                    prevPageNumber={pageNumber - 1}
                     content={content}
                 />),
             "I": (
                 <Introduction 
                     nextPageNumber={nextPageNumber}
-                    prevPageNumber={prevPageNumber}
                     content={content}
                 />),
             "R": (
                 <Reflection 
                     nextPageID={nextPageNumber}
-                    prevPageID={prevPageNumber}
+                    prevPageID={pageNumber - 1}
                     content={content}
                 />),
             "S": (
                 <Stakeholders 
                     nextPageID={nextPageNumber}
-                    prevPageID={prevPageNumber}
+                    prevPageID={pageNumber - 1}
                     content={content}
                 />)
         }
@@ -42,6 +41,7 @@ export default class Page {
         this.visited = visited;
         this.completed = completed;
         this.pageNumber = pageNumber;
+        this.title = title;
         this.html = (pageTypes[type]);
     }
 }

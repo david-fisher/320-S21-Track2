3340c2d49d08fe487329412cf8eae4f04a37aff2
kevinluo-hub <rongqiangluo@umass.edu>
2021-04-15 13:44:43 -0400

    added effect to go to the right page via submit, modified fetch

--- simulator/frontend/src/pages/components/checkbox.js
+++ simulator/frontend/src/pages/components/checkbox.js
@@ -25,11 +25,32 @@ const useStyles = makeStyles((theme) => ({
 export default function ErrorRadios(props) 
 {
   let nextPageTitle = "";
-  let   content_url = props.content_url;
+  let content_url = '/pages';
   const classes = useStyles();
   const [value, setValue] = React.useState('');
   const [error, setError] = React.useState(false);
   const [helperText, setHelperText] = React.useState('Choose carefully');
+  const [scenarios, setScenarios] = React.useContext(ScenariosContext);
+  const [choices, setChoices] = React.useState([]);
+
+  useEffect (() => {
+    fetch(BASE_URL + '/action_page/')
+    .then(res => res.json())
+    .then(actionData => {
+      let actionChoices = []
+      
+      actionData.results.filter(element => element.page === props.pageNumber).
+      forEach(element => {
+        let choice = {
+          text: element.choice,
+          result_page: element.result_page
+        }
+        actionChoices.push(choice)
+      });
+      console.log(actionChoices)
+      setChoices(actionChoices)
+    })
+  }, [])
 
   const handleRadioChange = (event) => {
     setValue(event.target.value);
@@ -43,13 +64,7 @@ export default function ErrorRadios(props)
     if (value !== '') {
       setHelperText('Good Answer!');
       setError(true);
-      // props.pages[props.nextPageName].completed = true;
-      // props.nextPage();
-      props.handleResponse(value).then(res => {
-        console.log("LETS GO TO NEXT PAGE")
-        props.pages[props.nextPageName].completed = true;
-        props.nextPage();
-      }).catch(err => alert(err))
+      props.changePage(choices.filter(choice => choice.text === value)[0].result_page)
     } else {
       console.log("FAIL TO GO TO NEXT PAGE")
       setHelperText('Please select an option.');
@@ -57,61 +72,62 @@ export default function ErrorRadios(props)
     }
   };
 
-  const [scenarios, setScenarios] = React.useContext(ScenariosContext);
-  const [choices, setChoices] = React.useState([]);
 
-  useEffect(() => {
-    // backend call
-    console.log("base: " + BASE_URL + " scenario: " + SCENARIO_ID + " student: " + STUDENT_ID);
-    axios({
-      method: 'get',
-      url: BASE_URL + content_url,
-      headers: {
-        scenarioID: scenarios.currentScenarioID,
-        studentID: STUDENT_ID,
-      }
-    }).then(response => {
-      console.log(response);
 
-      const x = [];
-      console.log(content_url);
-      if (scenarios.currentScenarioID == 1)
-      {
-        for (var i = 0; i < response.data[0].option.length; i++)
-          x.push({value:response.data[0].option_id[i], label: response.data[0].option[i]});
-        console.log(x);
-        setChoices(choices => x);
-      }
-      if ((scenarios.currentScenarioID == 2)){
-        for (var i = 0; i < response.data[1].option.length; i++)
-          x.push({value:response.data[1].option_id[i], label: response.data[1].option[i]});
-        console.log(x);
-        setChoices(choices => x);
-      }
-    }).catch((err)=>{
-      console.log("err",err);
-      //alert(err);
-    });
-  }, [scenarios])
 
-  function getNextPageTitle(props) {
-    switch (props.nextPageName) {
-      case "gatheredInformation": nextPageTitle = "Gathered Information";
-      break;
-      case "summary": nextPageTitle = "Summary";
-      break;
-      default:
-        break;
-    }
-  }
-  getNextPageTitle(props);
+
+  // useEffect(() => {
+  //   // backend call
+  //   console.log("base: " + BASE_URL + " scenario: " + SCENARIO_ID + " student: " + STUDENT_ID);
+  //   axios({
+  //     method: 'get',
+  //     url: BASE_URL + content_url,
+  //     headers: {
+  //       scenarioID: scenarios.currentScenarioID,
+  //       studentID: STUDENT_ID,
+  //     }
+  //   }).then(response => {
+  //     console.log(response);
+
+  //     const x = [];
+  //     console.log(content_url);
+  //     if (scenarios.currentScenarioID == 1)
+  //     {
+  //       for (var i = 0; i < response.data[0].option.length; i++)
+  //         x.push({value:response.data[0].option_id[i], label: response.data[0].option[i]});
+  //       console.log(x);
+  //       setChoices(choices => x);
+  //     }
+  //     if ((scenarios.currentScenarioID == 2)){
+  //       for (var i = 0; i < response.data[1].option.length; i++)
+  //         x.push({value:response.data[1].option_id[i], label: response.data[1].option[i]});
+  //       console.log(x);
+  //       setChoices(choices => x);
+  //     }
+  //   }).catch((err)=>{
+  //     console.log("err",err);
+  //     //alert(err);
+  //   });
+  // }, [scenarios])
+
+  // function getNextPageTitle(props) {
+  //   switch (props.nextPageName) {
+  //     case "gatheredInformation": nextPageTitle = "Gathered Information";
+  //     break;
+  //     case "summary": nextPageTitle = "Summary";
+  //     break;
+  //     default:
+  //       break;
+  //   }
+  // }
+  // getNextPageTitle(props);
 
   return (
     <form onSubmit={handleSubmit}>
       <FormControl component="fieldset" error={error} className={classes.formControl}>
         <RadioGroup aria-label="quiz" name="quiz" value={value} onChange={handleRadioChange}>
           {choices.map((choice, index) => (
-              <FormControlLabel value={choice.value.toString()} key={choice.value.toString()} control={<Radio />} label={choice.label} />
+              <FormControlLabel value={choice.text} key={index} control={<Radio />} label={choice.text} />
             ))
           }
         </RadioGroup>

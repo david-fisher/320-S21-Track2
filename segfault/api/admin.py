ab7835ecc905833b949a00601a64c100da5542fd
matteoBaeyens <44113777+matteoBaeyens@users.noreply.github.com>
2021-04-15 15:59:32 -0400

    Update admin.py
    Updates to allow extended testing

--- segfault/api/admin.py
+++ segfault/api/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from api.models import *
 # Register your models here.
 
-#admin.site.register(Course)
-#admin.site.register(Scenario)
+admin.site.register(Course)
+admin.site.register(Scenario)

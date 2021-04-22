3b3a946cb516bd6a49cd6054026d83eeb25490ae
Arthur Wang <artwang@umass.edu>
2021-04-22 13:51:14 -0400

    Quick Edits

--- moral_kombat_backend/lead/tables/urls.py
+++ moral_kombat_backend/lead/tables/urls.py
@@ -55,7 +55,7 @@ urlpatterns = [
     path('logistics', logistics_page.as_view()),
     path('multi_issue', multi_issue.as_view()),
     path('dashboard', dashboard_page.as_view()),
-    # path('flowchart', flowchart.as_view()),
+    path('flowchart', flowchart.as_view()),
     # path('student_info',student_info.as_view()),
     # path('student_responses',student_responses.as_view()),
 

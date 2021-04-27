de1920d4efafb1e117a1aa2fb02846a871432549
sdubois18 <samdubois18@icloud.com>
2021-04-27 09:05:21 -0400

    Got initial create function request working

--- moral_kombat_backend/lead/tables/views.py
+++ moral_kombat_backend/lead/tables/views.py
@@ -291,6 +291,16 @@ class IssuesViewSet(viewsets.ModelViewSet):
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['SCENARIO_ID', "NAME"]
 
+    def create(self, request, *args, **kwargs):
+        
+        serializer = IssuesSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class Action_pageViewSet(viewsets.ModelViewSet):
     queryset = ACTION_PAGE.objects.all()

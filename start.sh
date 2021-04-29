ffc58203c527a67aa398803240537d8c768dab5b
Judge Russell <judgerussell@umass.edu>
2021-04-29 13:10:53 -0400

    fixed start up shell script

---
+++ start.sh
@@ -0,0 +1,15 @@
+IP=$(hostname -i); echo ip=$IP
+sed -i "s/HOST_IP/${IP}/" /etc/shibboleth/shibboleth2.xml
+
+for i in /var/www/html/simulator/static/js/*; do
+    sed -i "s/ethisim2.cs.umass.edu/${ORIGIN_HOST}/" $i;
+done
+
+for i in /var/www/html/se/static/js/*; do
+    sed -i "s/ethisim2.cs.umass.edu/${ORIGIN_HOST}/" $i
+done
+
+echo "export SERVER_NAME=${SERVER_NAME}" >> /etc/environment
+echo "export SERVER_ADMIN=${SERVER_ADMIN}" >> /etc/environment
+echo "\n. /etc/environment" >> /etc/httpd/envvars
+httpd-shibd-foreground

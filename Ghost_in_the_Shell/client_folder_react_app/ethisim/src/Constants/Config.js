8209e0ce8e8f21026e65b3fa21737121a5bc68be
Judge Russell <judgerussell@umass.edu>
2021-04-29 13:16:08 -0400

    fixed config.js files again

--- Ghost_in_the_Shell/client_folder_react_app/ethisim/src/Constants/Config.js
+++ Ghost_in_the_Shell/client_folder_react_app/ethisim/src/Constants/Config.js
@@ -1,3 +1,3 @@
-const baseURL = 'http://ethisim2.cs.umass.edu:8000';
+const baseURL = 'http://localhost:8000';
 
 export { baseURL };
--- start.sh
+++ start.sh
@@ -2,11 +2,11 @@ IP=$(hostname -i); echo ip=$IP
 sed -i "s/HOST_IP/${IP}/" /etc/shibboleth/shibboleth2.xml
 
 for i in /var/www/html/simulator/static/js/*; do
-    sed -i "s/ethisim2.cs.umass.edu/${ORIGIN_HOST}/" $i;
+    sed -i "s/localhost/${ORIGIN_HOST}/" $i;
 done
 
 for i in /var/www/html/se/static/js/*; do
-    sed -i "s/ethisim2.cs.umass.edu/${ORIGIN_HOST}/" $i
+    sed -i "s/localhost/${ORIGIN_HOST}/" $i
 done
 
 echo "export SERVER_NAME=${SERVER_NAME}" >> /etc/environment

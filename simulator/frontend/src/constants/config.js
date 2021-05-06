377c802f580ad25549babc2e4d831c1cf73344d0
matteoBaeyens <44113777+matteoBaeyens@users.noreply.github.com>
2021-05-06 12:18:38 -0400

    Deleted to ignore changes

--- simulator/frontend/src/constants/config.js
+++
@@ -1,4 +0,0 @@
-export const BASE_URL = 'localhost:5000/api';
-export const SCENARIO_ID = 1;
-export const STUDENT_ID = "1";
-export const DEV_MODE = true;
--- simulator/frontend/src/pages/components/htmlRenderer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-
-const HTMLRenderer = ({html}) => {
-    const sanitizedHTML = html.replace(/onerror=/, '').replace(/<a .*onmouseover=.*>/, '<a>');
-    return (<div className={'sun-editor-editable'} dangerouslySetInnerHTML={{__html: sanitizedHTML}}></div>);
-}
-
-export default HTMLRenderer;
